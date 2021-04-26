01963a557fd5f3c61109ae1b3d9a04c841f91da8
Tommaso Soru <tom@tommaso-soru.it>
2021-04-27 00:45:57 +0100

    Trader-Binance interface & utils; updated notebook.

---
+++ app/binance_interface.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from binance.client import Client
+import configparser
+import math
+
+from app.utilities import *
+
+
+class BinanceInterface(object):
+
+    """docstring for BinanceInterface"""
+    def __init__(self, crypto, fiat):
+        self.crypto = crypto
+        self.fiat = fiat
+        self.symbol = f"{crypto}{fiat}"
+        self.configure()
+        self.step_size = self.get_step_size()
+
+
+    def configure(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        api_key = config.get('Binance', 'api_key')
+        api_secret = config.get('Binance', 'api_secret')
+        self.client = Client(api_key, api_secret)
+
+        
+    def get_step_size(self):
+        for f in self.client.get_symbol_info(self.symbol)['filters']:
+            if f['filterType'] == 'LOT_SIZE':
+                return float(f['stepSize'])
+
+
+    def get_all_orders(self):
+        return client.get_all_orders(symbol=self.symbol)
+
+
+    def free_balance(self, asset, fees=False):
+        balance = float(self.client.get_asset_balance(asset=asset)['free'])
+        if not fees:
+            return balance
+        else:
+            return (1 - FEES) * balance
+
+
+    def get_ticker_price(self):
+        return float(self.client.get_symbol_ticker(symbol=self.symbol)['price'])
+
+
+    def buy_all_in(self, at_price):
+
+        available_balance = self.free_balance(self.fiat, fees=True)
+
+        print(f"Available {self.fiat}: {available_balance}")
+
+        precision = int(round(- math.log(self.step_size, 10), 0))
+
+        print(f"{self.crypto} price in {self.fiat}: {at_price}")
+
+        qty = round(available_balance / at_price, precision)
+
+        print(f"{self.crypto} to buy: {qty}")
+
+        order = self.client.order_market_buy(
+            symbol=self.symbol,
+            quantity=qty
+        )
+
+        return order
--- app/strategies.py
+++ app/strategies.py
@@ -1,81 +1,103 @@
-def compute_macd_diff_peak_and_limit(df_orig,
-                                     take_profit=None,
-                                     stop_loss=None,
-                                     verbose=False,
-                                     plot_chart=False):
+#!/usr/bin/env python
+import numpy as np
+
+
+def compute_macd_diff_peak_and_limit(df_orig, take_profit=None, take_profit_bias=0, stop_loss=None):
+
     df = df_orig.copy()
 
-    if take_profit and stop_loss:  # static values
+    if take_profit is not None: # static values
         df['take_profit'] = take_profit
+    else:
+        assert 'take_profit' in df
+
+    if stop_loss is not None: # static values
         df['stop_loss'] = stop_loss
     else:
-        assert 'take_profit' in df and 'stop_loss' in df
+        assert 'stop_loss' in df
 
     df['suggest'] = 'IDLE'
-    df.loc[df['is_negative'] & df['is_upward'], 'suggest'] = 'BUY'
+    df.loc[df['is_negative'] & df['is_upward'] & (df['take_profit'] < np.inf), 'suggest'] = 'BUY'
 
-    df['limit'] = 0
-    df.loc[df['suggest'] == 'BUY', 'limit'] = df['open'] * df['take_profit']
+    # with an adaptive take_profit, stop_loss might no longer be necessary, as take_profit will decrease with the price
+    df['take_profit'] = df['take_profit'] + take_profit_bias
 
-    df['stop'] = 0
-    df.loc[df['suggest'] == 'BUY', 'stop'] = df['open'] * df['stop_loss']
+    df['limit_zeroes'] = 0
+    df.loc[df['suggest'] == 'BUY', 'limit_zeroes'] = df['open'] * df['take_profit']
 
-    s = 'OUT'  # IN=invested, OUT=liquidated
+    limit_array = list(df['limit_zeroes'])
+    for i, x in enumerate(limit_array):
+        if i > 0 and x == 0:
+            limit_array[i] = limit_array[i - 1]
+    df['limit'] = limit_array
+    del df['limit_zeroes']
+
+    df['stop_zeroes'] = 0
+    df.loc[df['suggest'] == 'BUY', 'stop_zeroes'] = df['open'] * df['stop_loss']
+
+    stop_array = list(df['stop_zeroes'])
+    for i, x in enumerate(stop_array):
+        if i > 0 and x == 0:
+            stop_array[i] = stop_array[i - 1]
+    df['stop'] = stop_array
+    del df['stop_zeroes']
+
+    s = 'OUT' # IN=invested, OUT=liquidated
+    action_values = []
+    result_values = []
+
     assets = 1
-    limit = None
-    stop = None
-
     assets_values = []
-    actions = []
-    results = []
-    invest = []
+ 
     investment = None
+    investment_values = []
+
     investment_when = None
-    invest_when = []
+    investment_when_values = []
+
+    investment_open = None
+    investment_open_values = []
+
     for index, row in df.iterrows():
-        if s == 'IN':  # this depends on the previous state
+        if s == 'IN': # this depends on the previous state
             assets = assets / row['prev_open'] * row['open']
         if s == 'OUT' and row['suggest'] == 'BUY':
             s = 'IN'
-            limit = row['limit']
-            stop = row['stop']
-            actions.append('BUY')
+            action_values.append('BUY')
+            result_values.append(np.nan)
             assets = assets * (1 - FEES)
             investment = assets
+            investment_values.append(np.nan)
             investment_when = row['ds']
-            results.append(np.nan)
-            invest.append(np.nan)
-            invest_when.append(np.nan)
-        elif s == 'IN' and (row['open'] * (1 - FEES) - limit >= 0 or row['open'] <= stop):
+            investment_when_values.append(np.nan)
+            investment_open = row['open']
+            investment_open_values.append(investment_open)
+        elif s == 'IN' and (row['open'] * (1-FEES) >= row['limit'] or row['open'] <= row['stop']):
             s = 'OUT'
-            limit = None
-            stop = None
-            actions.append('SELL')
+            action_values.append('SELL')
+            result_values.append('GAIN' if assets >= investment else 'LOSS')
             assets = assets * (1 - FEES)
-            results.append('GAIN' if assets >= investment else 'LOSS')
-            invest.append(investment)
-            invest_when.append(investment_when)
+            investment_values.append(investment)
+            investment_when_values.append(investment_when)
+            investment_open_values.append(investment_open)
         else:
-            actions.append('----')
-            results.append(np.nan)
-            invest.append(np.nan)
-            invest_when.append(np.nan)
+            action_values.append('----')
+            result_values.append(np.nan)
+            investment_values.append(np.nan)
+            investment_when_values.append(np.nan)
+            investment_open_values.append(investment_open)
         assets_values.append(assets)
 
     df['assets'] = assets_values
-    df['action'] = actions
-    df['result'] = results
-    df['invest'] = invest
-    df['invest-when'] = invest_when
-    bnh = df['open'].iloc[-1] / df['open'].iloc[0]
+    df['action'] = action_values
+    df['result'] = result_values
+    df['investment'] = investment_values
+    df['invest_when'] = investment_when_values
+    df['invest_open'] = investment_open_values
 
-    if verbose:
-        display(df[:30])
-
-    if plot_chart:
-        df['buy-and-hold'] = df['open'] / df['open'].iloc[0]
-        plot('line', df, ['assets', 'buy-and-hold'],
-             title=f"{symbol} - {strategies[strategy]} take_profit={take_profit} stop_loss={stop_loss}")
+    comparable_from = df.index[df['take_profit'] < np.inf].to_list()[0]
+    print(f"Comparable from: {df['ds'].iloc[comparable_from]}")
+    bnh = df['open'].iloc[-1] / df['open'].iloc[comparable_from]
 
     return assets, bnh, df
 
